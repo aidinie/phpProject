8e8711a31c954707de04a6837bb4e896bcd991b8
nadaa <11552155@qq.com>
2017-01-15 22:53:04 +0800

    登录页面。

---
+++ day4review/login.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nad
+ * Date: 2017/1/11
+ * Time: 16:03
+ */
+include 'conn.php';
+if (isset($_GET['uri'])){
+    $uri=$_GET['uri'];
+
+}else{
+    $uri="index.php";
+}
+if (isset($_POST['sub'])){
+    $name=$_POST['name'];
+    $pass=$_POST['pass'];
+    $uri=$_POST['uri'];
+    $sql="select * from user where uname='$name' and pass='$pass'";
+    $query=mysqli_query($link,$sql);
+    $rs=mysqli_fetch_array($query);
+    if($rs){
+        setcookie('id',$rs['uid'],time()+160);
+        setcookie('name',$rs['uname'],time()+160);
+        echo "<script>location='$uri'</script>";
+    }
+}
+?>
+<meta charset="utf-8" >
+<form action="login.php" method="post">
+    <input type="hidden" name="uri" value="<?php echo $uri?>">
+    用户名:<input type="text" name="name"><br>
+    密&nbsp;码:<input type="password" name="pass"><br>
+    <input type="submit" name="sub" value="登录">
+</form>
