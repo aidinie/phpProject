b163fac3afdc483f70b00d29300c48913028b49d
nadaa <11552155@qq.com>
2017-04-08 19:37:48 +0800

    控制器

--- aaci/application/controllers/user.php
+++ aaci/application/controllers/user.php
@@ -14,13 +14,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           $this->load->model('user_model');
           $rs=$this->user_model->checkname($name);
           if($rs){
-              $this->reg();
+//              $this->reg();
+              redirect('user/reg');
           }else{
               $rs=$this->user_model->getinsert($name,$pwd);
               if($rs){
-                  $this->login();
+                  redirect('user/login');
+                  //$this->login();
               }else{
-                  $this->reg();
+                  redirect('user/reg');
+                 // $this->reg();
               }
           }
       }
@@ -36,6 +39,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
               echo "exist";
           }else{
               echo 'success';
+          }
+      }
+      public function index(){
+          $this->load->view('index.php');
+      }
+      public function dologin(){
+          $name=$this->input->post('uname');
+          $pwd=$this->input->post('pwd');
+          $this->load->model('user_model');
+          $rs=$this->user_model->get_sel($name,$pwd);
+          if($rs){
+
+              $newdata = array(
+                  'uname'  => $rs->uname,
+                  'uid'     => $rs->uid,
+              );
+
+
+              $this->session->set_userdata($newdata);
+           redirect('user/index');
           }
       }
   }
