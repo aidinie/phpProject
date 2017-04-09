50016d7b89902d8919398b9ba9d5a1f6e5eebff9
nadaa <11552155@qq.com>
2017-04-09 17:29:07 +0800

    主页

--- aaci/application/views/index.php
+++ aaci/application/views/index.php
@@ -9,5 +9,16 @@ login in
  */
 if($this->session->uid){
     echo ("<h3>".$this->session->uname."已登录"."</h3>");
+    header('user/index');
+}
+foreach ($bloglist as $v) {
+    ?>
+    <h3>标题:<?php echo $v->title?></h3>
+    <p>内容:<?php echo $v->content?></p><br/>
+    <p>时间:<?php echo $v->time?></p><br/>
+    <p>访问量:<?php echo $v->hits?></p>
+    <hr>
+    <?php
 }
 ?>
+<?php echo $this->pagination->create_links();?>
